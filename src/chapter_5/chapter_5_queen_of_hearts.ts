d2a6638272bbdd19541cc4ac99f2917b083a55ef
sjmitch-git <sjmitch20@outlook.com>
2023-10-25 13:43:59 +0100

    Fix: all chapters

--- src/chapter_5/chapter_5_queen_of_hearts.ts
+++ src/chapter_5/chapter_5_queen_of_hearts.ts
@@ -1,51 +1,58 @@
-import { endAdventure } from '../..';
-import { wakeUp } from '../chapter_6/chapter_6_wake_up';
-import { askQuestion, clear, print } from '../ui/console';
+import { endAdventure } from "../..";
+import { wakeUp } from "../chapter_6/chapter_6_wake_up";
+import { askQuestion, clear, print } from "../ui/console";
 
-const VERDICTS = ['Guilty', 'Not Guilty'] as const;
-type Verdict = typeof VERDICTS[number];
+const VERDICTS = ["Guilty", "Not Guilty"] as const;
+type Verdict = (typeof VERDICTS)[number];
 
 interface Witness {
-	name: string;
-	giveEvidence: () => Verdict;
+  name: string;
+  giveEvidence: () => Verdict;
 }
 
 export function meetTheQueen(): void {
-	clear(true);
-	print('The Queen has put you on trial for stealing tarts.');
+  clear(true);
+  print("The Queen has put you on trial for stealing tarts.");
 
-	let guilty: boolean = false;
+  let guilty: boolean = false;
 
-	let witnesses: Witness[] = []; // 👉 FIXME ❌ - call getWitnesses here
+  let witnessNames: string[] = [
+    "The Mad Hatter",
+    "The March Hare",
+    "The Cheshire Cat",
+    "The White Rabbit",
+  ];
 
-	if (!witnesses || witnesses.length === 0) {
-		print(`No witnesses have come forward to defend you.`);
-		guilty = true;
-	}
+  let witnesses: Witness[] = getWitnesses(witnessNames); // 👉 FIXME ❌ - call getWitnesses here
 
-	let witnessCount = 0;
+  if (!witnesses || witnesses.length === 0) {
+    print(`No witnesses have come forward to defend you.`);
+    guilty = true;
+  }
 
-	witnesses.forEach((witness) => {
-		witnessCount++;
-		print(
-			`${witness.name} gives their evidence: ${witness.giveEvidence()}`
-		);
-		if (witness.giveEvidence() === 'Guilty') {
-			guilty = true;
-		}
-	});
+  let witnessCount = 0;
 
-	if (witnessCount < 4 || guilty) {
-		print(`You have been found guilty! "Off with her head!" 😱`);
-		return endAdventure();
-	} else {
-		print(`You have been found NOT GUILTY! Thank goodness. 🥳`);
-		print('Time to wake up...');
-		return askQuestion('Press ENTER to continue! ', wakeUp);
-	}
+  witnesses.forEach((witness) => {
+    witnessCount++;
+    print(`${witness.name} gives their evidence: ${witness.giveEvidence()}`);
+    if (witness.giveEvidence() === "Guilty") {
+      guilty = true;
+    }
+  });
+
+  if (witnessCount < 4 || guilty) {
+    print(`You have been found guilty! "Off with her head!" 😱`);
+    return endAdventure();
+  } else {
+    print(`You have been found NOT GUILTY! Thank goodness. 🥳`);
+    print("Time to wake up...");
+    return askQuestion("Press ENTER to continue! ", wakeUp);
+  }
 }
 
 // 👉 FIXME ❌ - this function needs writing to meet the above criteria
-function getWitnesses(): any {
-	return [];
+function getWitnesses(witnessNames: string[]): Witness[] {
+  return witnessNames.map((n) => {
+    return { name: n, giveEvidence: () => "Not Guilty" };
+  });
 }
